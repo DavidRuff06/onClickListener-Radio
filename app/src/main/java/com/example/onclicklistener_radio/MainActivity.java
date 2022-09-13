c03edf8362de1eb9fc129d96a8d615c5572b2720
DavidRuff06 <90778233+DavidRuff06@users.noreply.github.com>
2022-09-13 12:55:51 -0500

    Completed

--- app/src/main/java/com/example/onclicklistener_radio/MainActivity.java
+++ app/src/main/java/com/example/onclicklistener_radio/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.onclicklistener_radio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+        public void radioClick(View view) {
+            TextView question = findViewById(R.id.question);
+            boolean checked = ((RadioButton) view).isChecked();
+
+            switch (view.getId()){
+                case R.id.radio_one:
+                    if(checked) {
+                        question.setText("CELEBRATE!!!");
+                    }
+                    break;
+                case R.id.radio_two:
+                    if(checked) {
+                        question.setText("Glad to hear it!!!");
+                    }
+                    break;
+                case R.id.radio_three:
+                    if(checked) {
+                        question.setText("Take a deep breath\nit will all be ok");
+                    }
+                    break;
+                case R.id.radio_four:
+                    if(checked) {
+                        question.setText("Go talk to a trusted\nadult");
+                    }
+                    break;
+
+            }
+        }
+    }
